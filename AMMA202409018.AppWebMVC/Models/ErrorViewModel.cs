67e9c404393ea3d07eb0a47a1ec21cee4b2f7fbd
Ángel Miguel Mena Argumedo <angelmeba4@gmail.com>
2024-09-05 01:56:20 -0600

    Agregacion Inicial Del MVC

---
+++ AMMA202409018.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AMMA202409018.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
